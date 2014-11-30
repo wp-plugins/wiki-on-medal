92b1cc7b2e811676be72fd229325391419279bc8
Piotr Pesta <Piotr Pesta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-30 21:31:12 +0000

    Tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wiki-on-medal/tags/1.0.0@1035645 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,77 @@
+<?php
+
+function wikiPL() {
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,"http://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna");
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_MAXREDIRS,10);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,70);
+		curl_setopt($ch,CURLOPT_USERAGENT,"wiki on medal");
+		curl_setopt($ch,CURLOPT_HTTP_VERSION,'CURLOPT_HTTP_VERSION_1_1');
+	$data = curl_exec($ch);
+	$data = strip_tags($data); //wywala ca³y html
+	$data = trim(preg_replace('/\s+/', ' ', $data)); //usuwa wszystkie, niepotrzebne przerwy, entery itp.
+	$start = "medal";
+	$end = "Czytaj";
+
+	function getTextBetweenTags($start, $end, $data){
+ 		$matches = array();
+    	$pattern = "/$start(.*?)$end/";
+    	if (preg_match($pattern, $data, $matches)) {
+			echo "<div class=\"wikimedal\">" . $matches[1] . "</div>";
+		}
+ 	}
+
+	return getTextBetweenTags($start, $end, $data);
+}
+
+function wikiEN() {
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,"http://en.wikipedia.org/wiki/Main_Page");
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_MAXREDIRS,10);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,70);
+		curl_setopt($ch,CURLOPT_USERAGENT,"wiki on medal");
+		curl_setopt($ch,CURLOPT_HTTP_VERSION,'CURLOPT_HTTP_VERSION_1_1');
+	$data = curl_exec($ch);
+	$data = strip_tags($data); //wywala ca³y html
+	$data = trim(preg_replace('/\s+/', ' ', $data)); //usuwa wszystkie, niepotrzebne przerwy, entery itp.
+	$start = "featured article";
+	$end = "\(Full";
+
+	function getTextBetweenTags($start, $end, $data){
+ 		$matches = array();
+    	$pattern = "/$start(.*?)$end/";
+    	if (preg_match($pattern, $data, $matches)) {
+			echo "<div class=\"wikimedal\">" . $matches[1] . "</div>";
+		}
+ 	}
+
+	return getTextBetweenTags($start, $end, $data);
+}
+
+function wikiDE() {
+	curl_setopt($ch,CURLOPT_URL,"http://de.wikipedia.org/wiki/Wikipedia:Hauptseite");
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_MAXREDIRS,10);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,70);
+		curl_setopt($ch,CURLOPT_USERAGENT,"wiki on medal");
+		curl_setopt($ch,CURLOPT_HTTP_VERSION,'CURLOPT_HTTP_VERSION_1_1');
+	$data = curl_exec($ch);
+	$data = strip_tags($data); //wywala ca³y html
+	$data = trim(preg_replace('/\s+/', ' ', $data)); //usuwa wszystkie, niepotrzebne przerwy, entery itp.
+	$start = "des Tages";
+	$end = 'â€“ Zum'; // em dash w stanie czystym
+
+	function getTextBetweenTags($start, $end, $data){
+ 		$matches = array();
+    	$pattern = "/$start(.*?)$end/";
+    	if (preg_match($pattern, $data, $matches)) {
+			echo "<div class=\"wikimedal\">" . $matches[1] . "</div>";
+		}
+ 	}
+
+	return getTextBetweenTags($start, $end, $data);
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Plugin Name ===
+Plugin Name: Wiki on Medal
+Version: 1.0.0
+Plugin URI: http://smartfan.pl/
+Tags: widget, wikipedia, article on medal, wikipedia widget
+Requires at least: 2.8.0
+Tested up to: 4.0.1
+Stable tag: 1.0.0
+PHP Version: 5.2.9
+Author: Piotr Pesta
+
+== Description ==
+
+Wiki on Medal lets your users know see article on medal from Wikipedia. Supported languages so far: Deutsch, Polish, English.
+
+== Installation ==
+
+1. Upload the zip to 'plugins' directory
+2. Unzip (steps 1 and 2 cand also be performed automatically)
+3. Activate the plugin
+4. Configure and place the widget on your sidebar
+
+Or just add .zip file as a new plugin in your Wordpress administration panel.
+
+
+== Changelog ==
+* 1.0
+Initial Release
